2605daf7d789eb61b86012db2be98986558e1a4b
SRNelson1995 <oscarnelson15@yahoo.com>
2018-06-08 15:38:36 -0600

    app

---
+++ SystemBec/app/Http/Controllers/TallerController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use DB;
+
+class TallerController extends Controller
+{
+     /*Este constructor lo que hace es pasarme a espanol la fecha con carbon*/
+    public function __construct(){
+           Carbon::setLocale('es');
+                                 }
+
+    /*Sirve para la creacion del taller pantalla donde se crea con todos los campos*/
+    public function create(){
+    return view('templates.admin.formacion.talleres.agregar');
+						                }
+
+ 
+    /*Crea el taller*/                        
+	public function store(Request $request){ 
+    		   
+	return redirect()->route('templates.admin.formacion.talleres.index');
+							                             }
+
+	/*Pagina donde se muestran todas las taller*/
+	public function index(Request $request){
+		       $carbon=Carbon::now();
+       
+	return view('templates.admin.formacion.talleres.index')->with('carbon',$carbon);
+	                                          }
+
+    /*Muestra todos los talleres que ya fueron desactivad0s*/                                        
+	public function historial(Request $request){
+		
+	return view('templates.admin.actividades.historial')->with('actividades',$actividades);
+	                                             }
+
+    /*Guardo cambios en los talleres*/
+  	public function update(Request $request, $id){
+
+    return redirect()->route('Actividades.index');
+                                                 }
+
+    /*Edito los talleres*/                                             
+    public function edit($id){
+     	     
+    return view('templates.admin.actividades.editar');
+                             }
+
+    /*Este metodo es de eliminar pero yo lo utilice para cambiar el estado de los talleres ('sin son desactivados o no')*/
+    public function destroy($id){     	
+           
+    return redirect()->route('Actividades.index');
+
+		}
+
+}
